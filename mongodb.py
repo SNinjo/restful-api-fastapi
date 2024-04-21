e30bd4f6e1932140a296b97c3d3293772fa62844
jo <jo.liao@dgiots.com>
2024-04-21 13:40:44 +0800

    fix: fix typo

--- mongodb.py
+++ mongodb.py
@@ -5,6 +5,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 load_dotenv()
 
-client = AsyncIOMotorClient(os.environ.get('MONGO_URL'))
+client = AsyncIOMotorClient(os.environ.get('MONGO_URI'))
 client.get_io_loop = asyncio.get_running_loop
 database = client['restful-api'] if 'pytest' not in sys.modules else client['test']
